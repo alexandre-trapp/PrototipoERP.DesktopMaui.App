40922777eae32cef936a5899504ada2bd015baca
Alexandre <alexandre.trapp@hotmail.com>
2021-12-20 18:39:03 -0300

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Essentials;
+
+namespace PrototipoERP.DesktopMaui
+{
+	public partial class MainPage : ContentPage
+	{ 
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		private void OnLoginClicked(object sender, EventArgs e)
+		{
+			//SemanticScreenReader.Announce(CounterLabel.Text);
+		}
+	}
+}
