0fd6bc9ffd404caebc1c5f99a1d4d8e7b47c77c2
Alexandre <alexandre.trapp@hotmail.com>
2021-12-21 13:38:12 -0300

    add buttons, list lembretes

--- Pages/LembretesPage.xaml.cs
+++ Pages/LembretesPage.xaml.cs
@@ -9,18 +9,18 @@ namespace PrototipoERP.DesktopMaui.Pages
 		{
 			InitializeComponent();
 
-			myListView.ItemsSource = AppState.Lembretes;
-			AppState.Lembretes.Add(new Models.LembreteModel() { UsuarioId = 1, DataHora = DateTime.Now, Texto = "aa" });
+			//myListView.ItemsSource = AppState.Lembretes;
+			//AppState.Lembretes.Add(new Models.LembreteModel() { UsuarioId = 1, DataHora = DateTime.Now, Texto = "aa" });
 		}
 
-		public void OnMeuUsuarioClicked(object sender, EventArgs args)
-		{
-			//AppState.Lembretes.Add(new TodoModel() { Id = 2, Title = "Ir a academia", CreatedAt = DateTime.Now, Done = false });
-		}
+        public void OnMeuUsuarioClicked(object sender, EventArgs args)
+        {
+            //AppState.Lembretes.Add(new TodoModel() { Id = 2, Title = "Ir a academia", CreatedAt = DateTime.Now, Done = false });
+        }
 
-		public void OnTodosUsuariosClicked(object sender, EventArgs args)
-		{
-			//AppState.Lembretes.Add(new TodoModel() { Id = 2, Title = "Ir a academia", CreatedAt = DateTime.Now, Done = false });
-		}
-	}
+        public void OnTodosUsuariosClicked(object sender, EventArgs args)
+        {
+            //AppState.Lembretes.Add(new TodoModel() { Id = 2, Title = "Ir a academia", CreatedAt = DateTime.Now, Done = false });
+        }
+    }
 }
